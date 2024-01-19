3c2258e495786a2c6da95d29b2d5161b2b7460c3
Michael <michaeldkim1@gmail.com>
2024-01-19 13:07:32 -0500

    Added tailwind css and complete basic foundation of todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
+
 function Todo() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const addTask = () => {
+        if (newTask.trim() !== '') {
+            setTasks([...tasks, {text: newTask, completed: false}]);
+            setNewTask('');
+        }
+    }
+
+    const toggleCompleted = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
+        setTasks(updatedTasks);
+    }
+
     return (
-      <div className="App">
-        <h1>Todo</h1>
+      <div className="container mx-auto p-4">
+        <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleCompleted(index)}
+            />
+            <label>{task.text}</label>
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
