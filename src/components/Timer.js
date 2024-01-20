c10b634640a1d58fa07f804af1dbefa7b2a982c9
Michael <michaeldkim1@gmail.com>
2024-01-19 21:16:56 -0500

    styles edit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,50 +2,54 @@
 import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
-  const initialMinutes = 25;
-  const [minutes, setMinutes] = useState(initialMinutes);
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
+    const initialMinutes = 25;
+    const [minutes, setMinutes] = useState(initialMinutes);
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    let interval;
+    useEffect(() => {
+        let interval;
 
-    if (isActive) {
-      interval = setInterval(() => {
-        if (seconds > 0) {
-          setSeconds((prevSeconds) => prevSeconds - 1);
-        } else if (minutes > 0) {
-          setMinutes((prevMinutes) => prevMinutes - 1);
-          setSeconds(59);
-        } else {
-          clearInterval(interval);
-          setIsActive(false);
+        if (isActive) {
+            interval = setInterval(() => {
+                if (seconds > 0) {
+                    setSeconds((prevSeconds) => prevSeconds - 1);
+                } else if (minutes > 0) {
+                    setMinutes((prevMinutes) => prevMinutes - 1);
+                    setSeconds(59);
+                } else {
+                    clearInterval(interval);
+                    setIsActive(false);
+                }
+            }, 1000);
         }
-      }, 1000);
-    }
 
-    return () => clearInterval(interval);
-  }, [isActive, minutes, seconds]);
+        return () => clearInterval(interval);
+    }, [isActive, minutes, seconds]);
 
-  const toggleTimer = () => {
-    setIsActive((prevIsActive) => !prevIsActive);
-  };
+    const toggleTimer = () => {
+        setIsActive((prevIsActive) => !prevIsActive);
+    };
 
-  const resetTimer = () => {
-    setIsActive(false);
-    setMinutes(initialMinutes);
-    setSeconds(0);
-  };
+    const resetTimer = () => {
+        setIsActive(false);
+        setMinutes(initialMinutes);
+        setSeconds(0);
+    };
 
-  return (
-    <div>
-      <p>{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</p>
-      <button onClick={toggleTimer}>
-        {isActive ? 'Pause' : 'Start'}
-      </button>
-      <button onClick={resetTimer}>Reset</button>
-    </div>
-  );
+    return (
+        <div className='flex-col'>
+            <div className='flex justify-center'>
+                <p>{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</p>
+            </div>
+            <div className='space-x-2'>
+                <button onClick={toggleTimer}>
+                    {isActive ? 'Pause' : 'Start'}
+                </button>
+                <button onClick={resetTimer}>Reset</button>
+            </div>
+        </div>
+    );
 };
 
 export default Timer;
