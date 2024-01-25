e9a852b6afb1c824d8574bfb0303b290c17ffe29
Michael <michaeldkim1@gmail.com>
2024-01-25 15:07:42 -0500

    Updated redesign

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Todo.css'
 
 function Todo() {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
+    const [dateTime, setDateTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDateTime(new Date());
+        }, 1000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const formattedDate = dateTime.toLocaleDateString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+    });
+    const formattedTime = dateTime.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true,
+    });
 
     const addTask = () => {
         if (newTask.trim() !== '') {
@@ -18,12 +38,18 @@ function Todo() {
         setTasks(updatedTasks);
     };
 
+    const toggleCompleted = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
+        setTasks(updatedTasks);
+    };
+
     return (
         <div className='todo-body'>
             <div className='todo-header'>
-                <h1>Todo List</h1>
+                <h1>{formattedDate} {formattedTime}</h1>
                 <div className='todo-addtask'>
-                    <button onClick={addTask}>Add Task</button>
+                    <button onClick={addTask}>+</button>
                     <input
                         type="text"
                         value={newTask}
@@ -37,16 +63,21 @@ function Todo() {
                     />
                 </div>
                 <div className='todo-divider' />
-            </div>
-            <div className='todo-tasklist'>
-                <ul>
-                    {tasks.map((task, index) => (
-                        <li key={index}>
-                            <span>{task.text}</span>
-                            <button onClick={() => removeTask(index)}>X</button>
-                        </li>
-                    ))}
-                </ul>
+                <div className='todo-tasklist'>
+                    <ul>
+                        {tasks.map((task, index) => (
+                            <li key={index} className={task.completed ? 'completed' : ''}>
+                                <input
+                                    type="checkbox"
+                                    checked={task.completed}
+                                    onChange={() => toggleCompleted(index)}
+                                />
+                                <span>{task.text}</span>
+                                <button onClick={() => removeTask(index)}>X</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
